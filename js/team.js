45c479d5c2f747bbe53fa6d09a8131197faddcef
neizerjemima <113371239+neizerjemima@users.noreply.github.com>
2023-03-31 22:21:10 -0400

    update

--- js/team.js
+++ js/team.js
@@ -2,16 +2,24 @@ let member = {
     Leila: {
         profile:"Nullam lobortis, turpis lobortis placerat vehicula, ipsum lacus bibendum dolor. sit amet pretium odio magna eget risus. Phasellus iaculis ultricies duiNullam lobortis, turpis lobortis placerat vehicula, ipsum lacus bibendum dolor, sit amet pretium odio magna get risus.",
         education: "Education" ,
-        date: " 2022 - date 2014 - 2019  2013 - 2014",
-        courses: "Interactive Media Design, Bachelor of Textiles, Graphic Design Diploma",
+        first: "2022 - date",
+        second: "2014 - 2019",
+        third: "2013 - 2014",
+        first_course: "Graphic Design Diploma",
+        second_course: "Bachelor of Textiles",
+        third_course: "Interactive Media Design",
         avatar: "team-member-leila.png"
     },
 
     Jemima: {
         profile:"Nullam lobortis, turpis lobortis placerat vehicula, ipsum lacus bibendum dolor. sit amet pretium odio magna eget risus. Phasellus iaculis ultricies duiNullam lobortis, turpis lobortis placerat vehicula, ipsum lacus bibendum dolor, sit amet pretium odio magna get risus.",
         education: "Education" ,
-        date: " 2022 - date 2014 - 2019  2013 - 2014",
-        courses: "Interactive Media Design, Bachelor In Multimedia Textiles, Graphic Design Diploma",
+        first: "2022 - date",
+        second: "2022 - 2021",
+        third: "2018 - 2021",
+        first_course: "Graphic Design Diploma",
+        second_course: "Bachelor In Multimedia",
+        third_course: "Interactive Media Design",
         avatar: "team-member-Jemima.jpg"
     }
 
